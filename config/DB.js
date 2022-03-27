d4c0bf0db8a0a74a7bf803c123bbcbb7eca5c592
Xhevrije Neziri <xhevrijeneziri1@gmail.com>
2022-03-28 01:09:17 +0200

    Project

---
+++ config/DB.js
@@ -0,0 +1,5 @@
+module.exports = {
+    DB: 'mongodb://root:PuCj3HWzxO6MIq9K@blog-shard-00-00.4hmtk.mongodb.net:27017,blog-shard-00-01.4hmtk.mongodb.net:27017,blog-shard-00-02.4hmtk.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-lw405g-shard-0&authSource=admin&retryWrites=true&w=majority'
+};
+
+//PuCj3HWzxO6MIq9K
