a18248d721f11fb1e05f09ec17b30c74bd82614b
gdallku <gezim.d@live.com>
2022-04-04 13:23:24 +0200

    reviews

--- src/components/guest/menu/Reviews.vue
+++ src/components/guest/menu/Reviews.vue
@@ -3,15 +3,15 @@
             <div class="title">
                 <h3> Take a look about what our guests have been saying about us</h3> 
             </div>
-            <div class="content">
+            <div class="content" >
               
-                    <div class="review">
+                    <div class="review" v-for="(testimonials, index) in testimonials" :key="index" >
                         <div class="head-review">
                             <img src="@/components/guest/img/profile.jpg" width="250px" alt="">
                         </div>
                         <div class="body-review">
-                            <div class="name-review">Jane Doe</div>
-                            <div class="place-review">Lorem Ipsum</div>
+                            <div class="name-review" >Jane Doe</div>
+                            <!-- <div class="place-review">Lorem Ipsum</div> -->
                             <div class="rating">
                                 <i class="fas fa-star"></i>
                                 <i class="fas fa-star"></i>
@@ -20,65 +20,54 @@
                                 <i class="fas fa-star"></i>
                             </div>
                             <div class="desc-review">
-                              <p>  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam animi eligendi nesciunt
-                                veniam, et
-                                magnam eius amet quia nihil perspiciatis qui illum aut excepturi aperiam officiis.
-                                Similique
-                                esse unde incidunt.</p>
+                            <p class="bold text-center px-md-3"> {{testimonials.name}}</p>
+
                             </div>
                         </div>
                     </div>
              
-                    <div class="review">
-                        <div class="head-review">
-                            <img src="@/components/guest/img/profile.jpg" width="250px" alt="">
-                        </div>
-                        <div class="body-review">
-                            <div class="name-review">Jane Doe</div>
-                            <div class="place-review">Lorem Ipsum</div>
-                            <div class="rating">
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                            </div>
-                            <div class="desc-review">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam animi eligendi nesciunt
-                                veniam, et
-                                magnam eius amet quia nihil perspiciatis qui illum aut excepturi aperiam officiis.
-                                Similique
-                                esse unde incidunt.</p>
-                            </div>
-                        </div>
-                    </div>
-               
-                
-                    <div class="review">
-                        <div class="head-review">
-                            <img src="@/components/guest/img/profile.jpg" width="250px" alt="">
-                        </div>
-                        <div class="body-review">
-                            <div class="name-review">Jane Doe</div>
-                            <div class="place-review">Lorem Ipsum</div>
-                            <div class="rating">
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star-half"></i>
-                            </div>
-                            <div class="desc-review">
-                             <p>   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam animi eligendi nesciunt
-                                veniam, et
-                                magnam eius amet quia nihil perspiciatis qui illum aut excepturi aperiam officiis.
-                                Similique
-                                esse unde incidunt.</p>
-                            </div>
-                        </div>
-                    </div>
+                   
                 </div>
         </div>
     
 
 </template>
+
+<script>
+
+export default {
+    data(){
+        return {
+            testimonials: [],
+        }
+    },
+
+    methods: {
+        fetchTest(){
+            this.axios.get('/Testimonials/')
+                .then(response => {
+                    this.testimonials = response.data;
+                });
+        },
+
+        deleteTest(id){
+            if (confirm('Are you sure you want to delete this Testimonials?')) {
+                this.axios.delete('/Testimonials/'+id)
+                .then(response => {
+                    console.log(response);
+                    window.location.reload();
+                });
+            }
+        }
+    },
+
+    mounted() {
+        this.fetchTest();
+    },
+}
+</script>
+<style scoped>
+.review {
+ margin: 10px;
+}
+</style>
--- src/components/user/umenu/Home.vue
+++ src/components/user/umenu/Home.vue
@@ -1,28 +1,26 @@
 <template>
+<div class="admin">
     <div class="container my-2">
         <div class="row mb-2">
-            <div class="col-md-6" v-for="post in reversedItems" :key="post.updated_date">
+            <div class="col-md-6 p-4" v-for="post in reversedItems" :key="post.updated_date">
                 <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                     <div class="col p-4 d-flex flex-column position-static">
                         <h3 class="mb-0" v-text="post.title"></h3>
-
                         <p class="card-text text-left" v-text="post.description"></p>
                         <p class="card-text text-left m-0" v-text="'Design: '+ post.design"></p>
                         <a :href="post.product_link" class="link mt-3">Product Link</a>
                     </div>
-
-                    <div class="col d-none d-lg-block">
+                    <div class="col-lg-3 col-md-4 col-sm-6 pt-2">
                         <img :src="`http://localhost:4000/posts/${post.image}`" class="img-thumbnail">
                     </div>
-
                     <div class="d-flex p-3 justify-content-between">
                         <div class="mb-1 text-muted">Posted: {{post.updated_date | human-date}}</div>
-                        
                        
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 </template>
 
@@ -65,10 +63,11 @@ export default {
 </script>
 
 
-<style lang="css">
-    /* @media (min-width: 768px){
-        .h-md-250 {
-            height: 250px;
-        }
-    } */
+<style scoped>
+.shadow-sm{
+    background-color:#dfd8dc ;
+}
+.admin{
+    margin-top: 123px;
+}
 </style>
